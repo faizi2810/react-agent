3b660c37d1195aeb92ebf724218cff00dae3c434
Faizan Ahmad <85900853+faizi2810@users.noreply.github.com>
2025-01-12 23:56:18 +0500

    Agent action
    Agent action

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import List, Union
 from dotenv import load_dotenv
 from langchain.agents import tool
 from langchain_core.prompts import PromptTemplate
@@ -7,13 +8,22 @@ from langchain_ollama import ChatOllama
 import os
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 load_dotenv()
+from langchain_core.agents import AgentAction, AgentFinish
+from langchain.tools import Tool
 
 
 @tool
 def get_text_length(text: str) -> int:
     """Returns the length of a text by characters"""
+    print(f"get_text_length enter with {text=}")
     text = text.strip("\n").strip('"')
     return len(text)
+
+def find_tool_by_name(tools: List[Tool], tool_name:str):
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    return ValueError(f"Tool with name {tool_name} not found")
 
 
 if __name__ == '__main__':
@@ -55,5 +65,13 @@ if __name__ == '__main__':
 
     agent = {"input": lambda x: x["input"]} | prompt_template | llm | ReActSingleInputOutputParser()
 
-    res = agent.invoke({"input": "What is the text length of 'Dog' in characters?"})
-    print(res)
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke({"input": "What is the length of 'Dog' in characters?"})
+    print(agent_step)
+    
+    if isinstance(agent_step,AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools,tool_name)
+        tool_input = agent_step.tool_input
+        
+        observation = tool_to_use.func(str(tool_input))
+        print(f"{observation=}")
